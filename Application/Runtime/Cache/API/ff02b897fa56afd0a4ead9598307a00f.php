36a206f86a5132d1423abe5824107eebeb090f38
zenght <zeng08@gmail.com>
2015-07-08 20:58:06 +0800

    debug

---
+++ Application/Runtime/Cache/API/ff02b897fa56afd0a4ead9598307a00f.php
@@ -0,0 +1,20 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html lang="en">
+<head>
+<meta content=" initial-scale=1, maximum-scale=1" name="viewport">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="format-detection" content="telephone=no"/>
+<meta charset="UTF-8">
+<title><?php echo ($_PAGE['title']); ?></title>
+<link rel="stylesheet" href="<?php echo ($host); ?>/Public/css/style.css">
+</head>
+<body>
+    <article class="con_box">
+        <div class="article_box">
+            <div>
+            <?php echo ($page); ?>
+            </div>
+        </div>
+    </article>
+</body>
+</html>
