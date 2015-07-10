43975e69998e24c000e3efae04c6f63c1786f1ad
zenght <zeng08@gmail.com>
2015-07-10 18:14:04 +0800

    feedback

--- Application/Runtime/Cache/Home/ef6ad9e7558ffd301d5cc85e6d029afc.php
+++ Application/Runtime/Cache/Home/ef6ad9e7558ffd301d5cc85e6d029afc.php
@@ -13,6 +13,7 @@
 ul{margin:0px;padding:0px;}
 li{list-style:none; margin-top:20px;text-align:center}
 .tips{line-height:40px;color:#666;font-size:12px;display:none}
+.btn-default{ font-size:14px; padding:10px 16px; width:50%}
 </style>
   </head>
 
@@ -22,7 +23,7 @@ li{list-style:none; margin-top:20px;text-align:center}
     <ul>
 <li><textarea class="form-control" rows="4" placeholder="有什么意见和想法尽管告诉我吧" id="feedback"></textarea></li>
 <li><input type="text" class="form-control" placeholder="请输入您的联系方式" id="contacts"></li>
-<li><button type="button" class="btn btn-default btn-lg" id="post">提交反馈信息</button></li>
+<li><button type="button" class="btn btn-default" id="post">提交反馈信息</button></li>
 <li><p class="tips bg-warning">请输入您的反馈意见</p></li>
     </ul>
     
