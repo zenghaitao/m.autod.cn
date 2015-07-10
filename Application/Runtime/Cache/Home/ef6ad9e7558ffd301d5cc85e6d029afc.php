13684e8f4eb5f0f3b257f3c9454f096c92a70191
zenght <zeng08@gmail.com>
2015-07-10 18:11:17 +0800

    HOME

--- Application/Runtime/Cache/Home/ef6ad9e7558ffd301d5cc85e6d029afc.php
+++ Application/Runtime/Cache/Home/ef6ad9e7558ffd301d5cc85e6d029afc.php
@@ -22,7 +22,7 @@ li{list-style:none; margin-top:20px;text-align:center}
     <ul>
 <li><textarea class="form-control" rows="4" placeholder="有什么意见和想法尽管告诉我吧" id="feedback"></textarea></li>
 <li><input type="text" class="form-control" placeholder="请输入您的联系方式" id="contacts"></li>
-<li><button type="button" class="btn btn-default" id="post">提交反馈信息</button></li>
+<li><button type="button" class="btn btn-default btn-lg" id="post">提交反馈信息</button></li>
 <li><p class="tips bg-warning">请输入您的反馈意见</p></li>
     </ul>
     
