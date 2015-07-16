e03962f1ab1ae071395ab3790798e4e2f1422986
zenght <zeng08@gmail.com>
2015-07-16 20:45:35 +0800

    comments

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -534,6 +534,7 @@ class IndexController extends BaseController  {
      */
     public function myComment(){
         $session_id = (int)$_GET['sessionId'];
+        $news_id = (int)$_GET['newsId'];
         $uid = $_SESSION['user_id'];
         
         $_PAGE['title'] = "我的评论";
@@ -542,7 +543,7 @@ class IndexController extends BaseController  {
         $M_news = new NewsModel();
         
         //热门评论
-        $comments = $M_news -> myCommentList($uid , 0 , 10);
+        $comments = $M_news -> myCommentList($uid , $news_id , 10);
         
         $this -> assign('news_id' , $news_id);
         $this -> assign('comments' , $comments);
