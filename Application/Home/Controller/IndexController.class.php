a35bc6c8d19056b148b9fe4402cc4c021e862ed6
zenght <zeng08@gmail.com>
2015-07-17 19:58:46 +0800

    news ad

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -272,6 +272,9 @@ class IndexController extends BaseController  {
         if($news['type'] == 'ad')
             $news['displayMode'] = 'C';
         $news['openMode'] = (string)$row['open_mode'];
+        if($news['imageCount'] > 1)
+            $news['openMode'] = 'image';
+            
         $news['gourl'] = (string)$row['gourl'];
         
         $news['favCount'] = (int)$row['fav_count'];
